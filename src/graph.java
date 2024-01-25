1bae839490ffce00301c221623fce23e26fecf12
Dhruvil_chotaliya <chotaliyadhruvil2309@gmail.com>
2024-01-25 09:07:43 +0530

    first commit

--- src/graph.java
+++ src/graph.java
@@ -21,6 +21,7 @@ public class graph {
         graph[0].add(new Edge(0, 1));
         graph[0].add(new Edge(0, 2));
 
+
         graph[1].add(new Edge(1, 3));
         graph[1].add(new Edge(1, 0));
 
